7b6800b4862d889f66f6e84d25e208a50f35a589
Chris <53177842+ChristopherMarais@users.noreply.github.com>
2025-02-10 00:40:30 -0500

    Astro update

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Christopher_Marais
 This is me...
 
+This Website is made using Astro.
+
 https://gcmarais.com/
 
 (In Progress)
---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  base: '/', // Use the root path since your custom domain points to the site's root.
+  // Other configuration options can go here
+});
+
+
