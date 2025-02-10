345da8fe4498f0201090ca521e80524c259fcc7e
Chris <53177842+ChristopherMarais@users.noreply.github.com>
2025-02-10 03:08:46 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
-// astro.config.mjs
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  base: '/', // Use the root path since your custom domain points to the site's root.
-  // Other configuration options can go here
-});
-
-
+  site: 'https://gcmarais.com',
+})
